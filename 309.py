278630837b5a29e1d634c3cc1008979b7a0dd8ad
Abdul Rahman Hashmi <rehmanabdul281@gmail.com>
2022-06-08 15:16:35 +0500

    list methods 2

---
+++ 309.py
@@ -0,0 +1,21 @@
+# using this list, 
+basket = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+# 1. Remove the Banana from the list
+basket.pop(0)
+print(basket)
+# 2. Remove "Blueberries" from the list.
+basket.pop()
+print(basket)
+# 3. Put "Kiwi" at the end of the list.
+basket.extend(["Kiwi"])
+print(basket)
+# 4. Add "Apples" at the beginning of the list
+basket.insert(0,"Apples")
+print(basket)
+# 5. Count how many apples in the basket
+
+print(basket.count("Apples"))
+# 6. empty the basket
+basket.clear()
+print(basket)
